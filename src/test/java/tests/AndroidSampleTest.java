34cfe1480de71bf624612b4f171a67fcafb2dbeb
junforpeace <bqsoapcream@gmail.com>
2022-06-17 08:57:36 +0200

    format corrected

--- src/test/java/tests/AndroidSampleTest.java
+++ src/test/java/tests/AndroidSampleTest.java
@@ -8,23 +8,22 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class AndroidSampleTest extends TestBase{
+public class AndroidSampleTest extends TestBase {
 
     @Test
     void searchTest() {
-    back();
-    step("Type search", () -> {
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Lurkmore");
+        back();
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Lurkmore");
         });
-    step("Удостовериться, что найдены варианты", () -> {
-        $$(AppiumBy.className("android.widget.TextView"))
-                .shouldHave(CollectionCondition.sizeGreaterThan(0));
-//
-    });
-    step("Перейти к статье о Луркморье", () -> {
-        $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-        $(AppiumBy.className("android.widget.TextView")).shouldHave(Condition.text("Lurkmore"));
-    });
+        step("Удостовериться, что найдены варианты", () -> {
+            $$(AppiumBy.className("android.widget.TextView"))
+                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
+        });
+        step("Перейти к статье о Луркморье", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+            $(AppiumBy.className("android.widget.TextView")).shouldHave(Condition.text("Lurkmore"));
+        });
     }
 }
