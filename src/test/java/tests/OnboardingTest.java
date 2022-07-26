45e70a2a66c0be59f6d20fc1b1159f3281803e8f
junforpeace <bqsoapcream@gmail.com>
2022-07-26 16:36:09 +0200

    new browserstack

---
+++ src/test/java/tests/OnboardingTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.back;
+import static io.qameta.allure.Allure.step;
+
+public class OnboardingTest extends TestBase{
+
+    @Test
+    void addLanguageTest() {
+
+        step("Add language", () -> {
+            $(AppiumBy.accessibilityId("ADD OR EDIT LANGUAGES")).click();
+            $(AppiumBy.accessibilityId("")).click();
+            $(AppiumBy.accessibilityId("Español")).click();
+            $(AppiumBy.accessibilityId("Español")).click();
+            $(AppiumBy.className("Español")).shouldHave(Condition.text("Español"));
+
+        });
+    }
+        @Test
+        void goNextStep() {
+            step("Info about settings", () -> {
+                $(AppiumBy.accessibilityId("Продолжить")).click();
+                $(AppiumBy.className("")).shouldHave(Condition.text(""));
+
+            });
+        }
+    @Test
+    void goThirdStep() {
+        step("Offer to join", () -> {
+            $(AppiumBy.accessibilityId("Продолжить")).click();
+            $(AppiumBy.className("")).shouldHave(Condition.text(""));
+            $(AppiumBy.className("")).shouldHave(Condition.text(""));
+
+        });
+    }
+        @Test
+        void goFourthStep() {
+            step("Send report", () -> {
+                $(AppiumBy.accessibilityId("Продолжить")).click();
+                $(AppiumBy.className("")).shouldHave(Condition.text(""));
+                $(AppiumBy.className("")).shouldHave(Condition.text(""));
+                $(AppiumBy.accessibilityId("")).click();
+                $(AppiumBy.accessibilityId("Search Wikipedia")).shouldBe(Condition.visible);
+            });
+
+        }
+
+}
